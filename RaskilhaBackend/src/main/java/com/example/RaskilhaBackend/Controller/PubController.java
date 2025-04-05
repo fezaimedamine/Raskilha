797996d094a4e1302ba7f3a858f343d2731f5b0c
الراشي مؤدم <moedem.errachi@etudiant-fst.utm.tn>
2025-04-05 19:00:50 +0100

    z

--- RaskilhaBackend/src/main/java/com/example/RaskilhaBackend/Controller/PubController.java
+++ RaskilhaBackend/src/main/java/com/example/RaskilhaBackend/Controller/PubController.java
@@ -4,7 +4,6 @@ import com.example.RaskilhaBackend.Service.PubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
